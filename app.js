04eb0f31309956617d00f7285cc7fc816ad6a97c
Abhishek <apabhishek2002@gmail.com>
2021-05-26 15:33:14 +0530

    fixed bugs

--- app.js
+++ app.js
@@ -6,40 +6,45 @@ const ul_active = document.querySelector(".ul-active");
 var isOpen = false;
 var count = 0;
 
-
 const lucky = document.querySelector(".inside-button");
 const burgers = document.querySelector(".burgers");
 
-contact.addEventListener("mouseout",() =>{
-    contact.classList.add("anime");
-    
-
+contact.addEventListener("mouseout", () => {
+  contact.classList.add("anime");
 });
 
-contact.addEventListener("click" ,() => {
-    window.location.href="mailto:apabhishekbusiness@gmail.com?subject=Main from my website";
+contact.addEventListener("click", () => {
+  window.location.href =
+    "mailto:apabhishekbusiness@gmail.com?subject=Main from my website";
 });
 
-lucky.addEventListener("click", ()=>{
-    console.log("got");
+lucky.addEventListener("click", () => {
+  console.log("got");
 });
-function doThis(){
-    ul.classList.toggle("ul-active");
-    contact.classList.toggle("contact");
-    // ul.classList.add("comeSlowly");
-    if(isOpen){
-        ul.classList.remove("comeSlowly");
-        ul.classList.add("goSlowly");
-        isOpen = false;
+function doThis() {
+  //   ul.classList.toggle("ul-active");
+  //   contact.classList.toggle("contact");
 
-    } else{
-        ul.classList.remove("goSlowly");
-        ul.classList.add("comeSlowly");
-        isOpen = true;
-    }
+  // ul.classList.add("comeSlowly");
+  console.log(isOpen);
+  if (isOpen) {
+    ul.classList.remove("comeSlowly");
+    ul.classList.add("goSlowly");
+
+    // same
+    setTimeout(() => {
+      ul.classList.remove("ul-active");
+      contact.classList.add("contact");
+    }, 300);
+    isOpen = false;
+  } else {
+    // same
+    ul.classList.add("ul-active");
+    contact.classList.remove("contact");
+
+    ul.classList.remove("goSlowly");
+    ul.classList.add("comeSlowly");
+    isOpen = true;
+  }
 }
-burgers.addEventListener("click",doThis);
-
-
-
-
+burgers.addEventListener("click", doThis);
